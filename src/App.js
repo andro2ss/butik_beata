27dde3c0815ca7707dd99ab4b454877a65cda8ad
andro2ss <a.brzeski@hotmail.com>
2022-02-01 05:25:08 -0800

    created galery carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
+
 import Header from "./components/common/header/Header";
 import Main from "./components/pages/Main";
 import Footer from "./components/common/footer/Footer";
 
 function App() {
+  const navElements = [
+    ["Strona główna", "stronaGlowna"],
+    ["Godziny otwarcia", "godzinyOtwarcia"],
+    ["Galeria", "galeria"],
+    ["Jak dojechać", "jakDojechac"],
+  ];
+
   return (
     <div className="App">
-      <Header />
+      <Header navItems={navElements} />
       <Main />
       <Footer />
     </div>
