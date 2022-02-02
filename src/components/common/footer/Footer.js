c70b73133ee9788a471a17297ab8c5490b555882
andro2ss <a.brzeski@hotmail.com>
2022-02-02 05:57:27 -0800

    created footer

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import { CContainer, CFooter, CLink } from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cibFacebook, cilArrowCircleTop } from "@coreui/icons";
+import "./Footer.scss";
 
 function Footer(props) {
-    return (
-        <div></div>
-    );
+  return (
+    <CFooter>
+      <CContainer xl>
+        <div className="footer__content">
+          <div>
+            <h2>Kontakt</h2>
+            <p>
+              <CLink href="https://coreui.io">
+                <CIcon icon={cibFacebook} size="xl" /> Butik Beata
+              </CLink>
+            </p>
+          </div>
+          <div>
+            <CLink href="#">
+              <CIcon icon={cilArrowCircleTop} size="xxl" />
+            </CLink>
+          </div>
+        </div>
+      </CContainer>
+    </CFooter>
+  );
 }
 
 export default Footer;
