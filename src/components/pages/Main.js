6c02bc37d69a2592bb8a1af54d9f9e3760957eb5
andro2ss <a.brzeski@hotmail.com>
2022-02-02 03:22:18 -0800

    created raw content version

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,152 +1,159 @@
 import React from "react";
-import { CCarousel, CCarouselItem, CContainer, CImage } from "@coreui/react";
+import {
+  CCarousel,
+  CCarouselItem,
+  CCol,
+  CContainer,
+  CImage,
+  CRow,
+} from "@coreui/react";
+import Iframe from "react-iframe";
+import CIcon from "@coreui/icons-react";
+import { cilLocationPin } from "@coreui/icons";
 
 function Main(props) {
   return (
     <CContainer xl>
-      <div id="stronaGlowna">asdasdas</div>
+      <div id="stronaGlowna">
+        <br />
+        <h1>Beata</h1>
+        <p>
+          Serdecznie zaprasza. Jesteśmy sklepem odzieżowym z modą damską i
+          akcesoriami. Nieprzerwanie świadczymy dla Państwa usługi od 200 roku.
+          Nasz personel z chęcią służyć będzie radą i pomocą. Staramy się iść z
+          trendami i mieć coś nowego dla Państwa chociaż raz w tygodniu.
+        </p>
+        <p>
+          Informacje o tymczasowych zmianach godzin pracy, jak i o dostawach
+          nowinek ze świata mody znajdą Państwo na naszym{" "}
+          <a href="https://www.facebook.com/">facebooku</a>.
+        </p>
+      </div>
+      <hr />
       <div id="godzinyOtwarcia">
-        asdsadasd asdasdasda Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Accusantium adipisci aliquam animi architecto assumenda at cumque
-        cupiditate delectus dicta, dolor dolore doloremque esse eum ex excepturi
-        explicabo facilis hic illo in incidunt iusto molestias mollitia
-        necessitatibus numquam porro quam quas quasi quis recusandae repellat,
-        repudiandae tempore totam unde ut voluptas voluptatem voluptates!
-        Aliquam aperiam beatae delectus deserunt dignissimos doloremque dolorum
-        ea, enim in incidunt laudantium maxime minus molestiae natus neque non
-        nostrum obcaecati placeat possimus quaerat quam quas saepe similique
-        sint temporibus tenetur vitae voluptates. Assumenda libero quod sit vel!
-        Aliquam ex expedita facere illo quam quos repellat temporibus veritatis,
-        voluptate? Beatae deserunt fugit inventore molestiae mollitia nesciunt,
-        nisi sunt ut voluptas voluptate? A adipisci asperiores commodi corporis
-        cum dignissimos distinctio ducimus eaque enim id iusto magni maxime nam
-        natus neque optio quaerat quas quia quidem quisquam repudiandae,
-        similique sint tenetur unde ut vitae voluptatum! Ad consequuntur
-        doloribus eum, iure magni quod rerum? Accusamus amet corporis deserunt
-        dolores, dolorum harum illum ipsam ipsum maiores nemo omnis quam quis,
-        quisquam recusandae, repellat? Alias cum dicta magni, molestias nulla
-        quibusdam quod totam! Aliquam amet autem blanditiis commodi culpa cumque
-        enim error esse eveniet facere fugit ipsum itaque iusto maxime nisi
-        nobis nostrum nulla numquam omnis optio perspiciatis quae quasi
-        recusandae rerum sed sequi sit soluta, vel vero voluptas! Animi dolores
-        enim ipsa iure maiores minus nemo perferendis sunt ut voluptate!
-        Accusantium assumenda blanditiis corporis culpa enim inventore labore
-        nesciunt omnis praesentium tenetur! Autem commodi culpa eos illo
-        inventore maiores, nostrum repellendus rerum saepe sequi totam ut vero.
-        Ad adipisci animi aperiam beatae commodi dignissimos dolore dolorem
-        ducimus earum explicabo fugiat harum ipsum iusto libero minima molestias
-        nam nesciunt officiis praesentium quas quos reiciendis rem temporibus,
-        ullam unde ut, voluptas voluptatibus. Aut consequatur culpa deserunt
-        dignissimos dolor et fugit labore laudantium, nesciunt officiis,
-        perferendis repudiandae, sed tenetur veniam voluptates! Commodi,
-        consectetur consequatur delectus dolor dolores eligendi ex fuga illum in
-        ipsam ipsum magnam magni necessitatibus nobis obcaecati officiis
-        possimus quidem quo, reiciendis reprehenderit repudiandae saepe
-        sapiente, tempore veniam veritatis voluptatem voluptates? Perferendis,
-        repellendus vitae! Accusamus alias amet consectetur cumque deleniti
-        dicta dolorem ea enim eos et eveniet, ex fuga fugit hic, illum iste iure
-        laboriosam modi nam non pariatur porro possimus quos repellendus
-        suscipit tempore temporibus unde. Aperiam culpa cumque eaque esse
-        excepturi molestiae quis ratione saepe sed temporibus! Ab amet
-        architecto autem cumque delectus deleniti impedit, iure labore minima
-        minus molestias nam, natus, nobis nostrum reiciendis suscipit tempore
-        voluptas voluptates. At atque cumque ea eaque excepturi, tempore totam
-        veniam? Cum et in officia quisquam sit? Ad consequuntur culpa debitis
-        delectus dignissimos dolor doloribus esse excepturi explicabo harum hic
-        illo illum iusto, laborum, magni modi molestiae, necessitatibus odit
-        officia possimus quo quod rem repellat sequi suscipit tempora tenetur ut
-        veritatis voluptates voluptatum! Adipisci aperiam enim et explicabo fuga
-        fugit impedit ipsam labore minus molestias nesciunt nostrum odio, optio,
-        placeat possimus quaerat quod quos repellendus repudiandae sit sunt vel
-        velit voluptatem. Beatae cum esse excepturi iure odio rerum! Inventore
-        itaque mollitia neque nulla recusandae.
+        <CContainer>
+          <p className="fs-3 text">Godziny Otwarcia</p>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Poniedziałek
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 17:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Wtorek
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 17:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Środa
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 17:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Czwartek
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 17:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Piątek
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 17:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Sobota
+            </CCol>
+            <CCol xs lg={2}>
+              10:00 - 14:00
+            </CCol>
+          </div>
+          <div className="row justify-content-md-center">
+            <CCol xs lg={2}>
+              Niedziela
+            </CCol>
+            <CCol xs lg={2}>
+              zamknięte
+            </CCol>
+          </div>
+        </CContainer>
       </div>
+      <hr />
+      <div id="galeria">
+        <p className="fs-3 text">Galeria</p>
+        <CCarousel controls transition="crossfade">
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src={require("./images/img1.jpg")}
+              alt="slide 1"
+            />
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src={require("./images/img2.jpg")}
+              alt="slide 2"
+            />
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src={require("./images/img3.jpg")}
+              alt="slide 3"
+            />
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src={require("./images/img4.jpg")}
+              alt="slide 4"
+            />
+          </CCarouselItem>
+        </CCarousel>
+      </div>
+      <hr />
+      <div id="jakDojechac"></div>
+      <p className="fs-3 text">Jak dojechać</p>
+      <CContainer>
+        <div className="row justify-content-md-center">
+          <CCol sm="auto">
+            <span>
+              <CIcon icon={cilLocationPin} size="md" />
+              Róża Wiatrów <br />
+              poziom: parter <br />
+              <br />
+              Promienna 7c <br />
+              Włocławek 87-800 <br />
+            </span>
+          </CCol>
+          <CCol sm="auto">
+            <Iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d604.9064391813467!2d19.039696729268623!3d52.66673699874015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471c990f5dd4e7a9%3A0xf1f663ea581e21d1!2sPromienna%207C%2C%2087-816%20W%C5%82oc%C5%82awek!5e0!3m2!1spl!2spl!4v1643798804739!5m2!1spl!2spl"
+              width="350"
+              height="300"
+              style="border:0;"
+              allowFullScreen=""
+              loading="lazy"
+            ></Iframe>
+          </CCol>
+        </div>
+      </CContainer>
 
-      <CCarousel id="galeria" controls transition="crossfade">
-        <CCarouselItem>
-          <CImage
-            className="d-block w-100"
-            src={require("./images/img1.jpg")}
-            alt="slide 1"
-          />
-        </CCarouselItem>
-        <CCarouselItem>
-          <CImage
-            className="d-block w-100"
-            src={require("./images/img2.jpg")}
-            alt="slide 2"
-          />
-        </CCarouselItem>
-        <CCarouselItem>
-          <CImage
-            className="d-block w-100"
-            src={require("./images/img3.jpg")}
-            alt="slide 3"
-          />
-        </CCarouselItem>
-        <CCarouselItem>
-          <CImage
-            className="d-block w-100"
-            src={require("./images/img4.jpg")}
-            alt="slide 4"
-          />
-        </CCarouselItem>
-      </CCarousel>
-      <div id="jakDojechac">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A culpa
-        delectus dicta, iste minima possimus quia quos sapiente ullam veritatis?
-        Aperiam delectus dignissimos dolorem dolores ipsam ipsum, iusto,
-        mollitia quisquam recusandae repellendus, similique soluta ullam veniam?
-        Aspernatur beatae consequatur dolores eveniet expedita fuga in incidunt,
-        iure numquam praesentium provident, reiciendis suscipit tenetur?
-        Asperiores consequuntur dicta exercitationem impedit ipsum, maiores
-        nesciunt nihil odit, omnis, quod suscipit tempore ut voluptatem!
-        Aspernatur assumenda atque debitis dolorem eaque earum exercitationem
-        illo illum inventore ipsa ipsum laudantium natus omnis, placeat quae
-        sapiente similique soluta ut velit voluptas! Animi consequatur dolores
-        ea minus nesciunt, nihil quia rerum! Ea excepturi impedit inventore,
-        libero numquam quidem unde. Aliquam culpa cum cumque debitis deleniti
-        deserunt dolore, dolorum eius eum eveniet facilis fugit iste laborum
-        maxime nemo nihil nobis quasi qui quos rerum saepe sit vero, voluptate.
-        Consequuntur culpa deserunt excepturi facilis fuga inventore ipsam magni
-        maiores molestiae nihil, sed tempora tenetur voluptates? Impedit ipsam
-        itaque minima obcaecati placeat quae similique, unde. Exercitationem,
-        obcaecati, saepe! Ab ad alias aliquam animi, aut dignissimos doloremque
-        earum iusto nemo nesciunt non perferendis quo ratione rem ullam?
-        Accusamus atque corporis dignissimos dolor dolores error facere ipsa
-        nemo omnis provident, quaerat quam quia, quis reprehenderit tenetur vero
-        vitae. Alias distinctio dolore dolorem dolorum earum enim, fugit impedit
-        incidunt iure magni mollitia nam nemo perferendis perspiciatis, placeat
-        saepe similique! Alias animi atque blanditiis culpa cum delectus dolore
-        dolores dolorum earum eius enim, error eveniet illum inventore iusto
-        maiores modi neque nobis optio quasi quibusdam quisquam quos ratione
-        sint suscipit temporibus ullam vel veritatis voluptatem voluptatum. Ad
-        blanditiis commodi facilis ipsam ipsum nobis soluta veniam. Corporis
-        delectus ea earum enim, provident quis ratione vel! Consequatur dolores
-        error hic libero nihil odio odit placeat recusandae soluta totam. A ab
-        adipisci at commodi dignissimos distinctio dolorem dolores eos eveniet
-        ex facere facilis hic illum inventore maxime molestiae molestias
-        necessitatibus nesciunt non obcaecati odit optio placeat porro quaerat,
-        quos, reiciendis repellat sint suscipit temporibus ut vero vitae
-        voluptas voluptate! Amet asperiores sequi sunt. Aliquid blanditiis
-        cumque earum esse eum, excepturi facilis fugit illum in iste laudantium
-        molestias necessitatibus nisi nostrum porro rem repellat temporibus vel.
-        Animi excepturi exercitationem fuga ipsum maiores minus molestiae odit,
-        quis quos sit! Ab consequatur cumque explicabo fuga optio, soluta.
-        Doloremque et eveniet exercitationem iste nostrum. At dolores ducimus et
-        excepturi inventore ipsum laboriosam nihil nisi omnis optio placeat rem
-        reprehenderit, saepe sapiente tempore tenetur vitae? Amet animi aperiam,
-        consectetur dignissimos ex id ipsum laborum neque nisi nobis officia
-        pariatur placeat quasi quia repellat sit totam. Accusamus adipisci alias
-        beatae illum in magni pariatur quidem temporibus vero, voluptatum. Ab
-        atque, beatae culpa deleniti fugit illo minima neque odit, quo quos
-        rerum, similique sint totam. Accusantium ducimus excepturi laborum
-        nesciunt qui quia recusandae repudiandae? Ad aliquid aperiam aspernatur
-        at, delectus dolores eligendi esse, illum in inventore ipsa ipsam
-        nesciunt odio omnis possimus repellat sit ullam. Accusamus, aspernatur
-        debitis deleniti distinctio facere, fugit in, ipsa iste perferendis quas
-        repellendus sunt suscipit ut vel voluptatem? Dolore et laudantium magni?
-      </div>
+      <hr />
     </CContainer>
   );
 }
